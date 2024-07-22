fd1f256c635acd0c549c6d34d2658eb48f7402ab
candezab <candela.zabalet@hotmail.com>
2024-07-22 19:28:41 -0300

    Reglas del libro y puntajes

--- demo/src/main/java/com/sistexperto/dto/PacienteRequest.java
+++ demo/src/main/java/com/sistexperto/dto/PacienteRequest.java
@@ -14,4 +14,114 @@ public class PacienteRequest {
     @JsonProperty("sexo")
     private String sexo;
     
+    @JsonProperty("delirio")
+    private boolean delirio;
+
+    @JsonProperty("alucinaciones")
+    private String alucinaciones;
+
+    @JsonProperty("disgregacion")
+    private boolean disgregacion;
+
+    @JsonProperty("incoherencia")
+    private boolean incoherencia;
+
+    @JsonProperty("positivos")
+    private int positivos;
+
+    @JsonProperty("negativos")
+    private int negativos;
+
+    @JsonProperty("bizarro")
+    private boolean bizarro;
+
+    @JsonProperty("extravagante")
+    private boolean extravagante;
+
+    @JsonProperty("inadecuado")
+    private boolean inadecuado;
+
+    @JsonProperty("adelgazado")
+    private boolean adelgazado;
+
+    @JsonProperty("hipomimico")
+    private boolean hipomimico;
+
+    @JsonProperty("hiprosexia")
+    private boolean hiprosexia;
+
+    @JsonProperty("paraprosexia")
+    private boolean paraprosexia;
+
+    @JsonProperty("abulia")
+    private boolean abulia;
+
+    @JsonProperty("hipobulia")
+    private boolean hipobulia;
+
+    @JsonProperty("impulsiones")
+    private boolean impulsiones;
+
+    @JsonProperty("compulsiones")
+    private boolean compulsiones;
+
+    @JsonProperty("apraxia")
+    private boolean apraxia;
+
+    @JsonProperty("ataxia")
+    private boolean ataxia;
+
+    @JsonProperty("exopraxia")
+    private boolean exopraxia;
+
+    @JsonProperty("manierismo")
+    private boolean manierismo;
+
+    @JsonProperty("esterotipia")
+    private boolean esterotipia;
+
+    @JsonProperty("negativismo")
+    private boolean negativismo;
+
+    @JsonProperty("flexibilidadCerea")
+    private boolean flexibilidadCerea;
+
+    @JsonProperty("aplanamientoAfectivo")
+    private boolean aplanamientoAfectivo;
+
+    @JsonProperty("labilidad")
+    private boolean labilidad;
+
+    @JsonProperty("irritabilidad")
+    private boolean irritabilidad;
+
+    @JsonProperty("expansivo")
+    private boolean expansivo;
+
+    @JsonProperty("inapropiado")
+    private boolean inapropiado;
+
+    @JsonProperty("anhedonia")
+    private boolean anhedonia;
+
+    @JsonProperty("autismo")
+    private boolean autismo;
+
+    @JsonProperty("trastornoComunicacion")
+    private boolean trastornoComunicacion;
+
+    @JsonProperty("esquizoafectivo")
+    private boolean esquizoafectivo;
+
+    @JsonProperty("bipolarCaracPsicoticas")
+    private boolean bipolarCaracPsicoticas;
+
+    @JsonProperty("sustancias")
+    private boolean sustancias;
+
+    @JsonProperty("causaOrganica")
+    private boolean causaOrganica;
+
+    @JsonProperty("pensamientoSP") //pensamiento sin particularidades, lo pongo como boolean porque si es true es normal
+    private boolean pensamientoSP;
 }
