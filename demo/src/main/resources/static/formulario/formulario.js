1f3560b5020097baad5cb116ba7773c63b16079a
Bren <brendavelazquez1121@gmail.com>
2024-07-22 21:13:43 -0300

    MOCK carga datos paciente terminado

--- demo/src/main/resources/static/formulario/formulario.js
+++ demo/src/main/resources/static/formulario/formulario.js
@@ -42,19 +42,6 @@ function cerrarPopup() {
     }, 3000); // Muestra el mensaje durante 3 segundos
 }
 
-function mostrarContenedorImagen() {
-    console.log("mostrarContenedorImagen");
-    const contenedorImagen = document.getElementById("image-container");
-    contenedorImagen.style.display = "block !important"; // Mostrar el contenedor de imagen
-    
-    console.log("contenedorImagen.style.display: " + contenedorImagen.style.display);
-}
-
-function ocultarContenedorImagen() {
-    const contenedorImagen = document.getElementById("image-container");
-    contenedorImagen.style.display = "none"; // Ocultar el contenedor de imagen
-}
-
 function initializeButtons() {
     const buttonPairs = [
         { yes: 'transtorno-autista-si', no: 'transtorno-autista-no' },
@@ -64,16 +51,14 @@ function initializeButtons() {
         { yes: 'transtorno-bipolar-si', no: 'transtorno-bipolar-no' },
         { yes: 'estudios-si', no: 'estudios-no' },
         { yes: 'sustancias-si', no: 'sustancias-no' },
-        { yes: 'antecedentes-familiares-si', no: 'antecedentes-familiares-no' }
+        { yes: 'antecedentes-familiares-si', no: 'antecedentes-familiares-no' },
+        { yes: 'sintomas-positivos-duracion-mayor', no: 'sintomas-positivos-duracion-menor' },
+        { yes: 'sintomas-positivos-delirios-presencia', no: 'sintomas-positivos-delirios-ausencia' },
     ];
 
     buttonPairs.forEach(pair => {
         const btnYes = document.getElementById(pair.yes);
         const btnNo = document.getElementById(pair.no);
-        const estudiosSiButton = document.getElementById("estudios-si");
-        const estudiosNoButton = document.getElementById("estudios-no");
-        estudiosSiButton.addEventListener("click", mostrarContenedorImagen);
-        estudiosNoButton.addEventListener("click", ocultarContenedorImagen);
 
         if (btnYes && btnNo) {
             btnYes.addEventListener('click', function () {
@@ -81,14 +66,11 @@ function initializeButtons() {
                 btnNo.classList.remove('selected');
                 this.classList.add('selected');
 
-                // console.log("btnYes: " + this.id);
                 //Para que se desplieguen los campos de estudio
                 if (this.id === 'estudios-si') {
-                    // this.classList.add('selected');
                     $("#estudio-comentario").slideDown();
                     $("#estudio-causa-natural").slideDown();
-                    // $("#image-container").css('display', 'block');
-                    // $("#image-container").slideDown();
+                    $("#image-container").slideDown();
                 }
             });
 
@@ -99,17 +81,47 @@ function initializeButtons() {
 
                 //Para que se dejen de mostrar los campos de estudio
                 if (this.id === 'estudios-no') {
-                    // this.classList.add('selected');
                     $("#estudio-comentario").slideUp();
                     $("#estudio-causa-natural").slideUp();
-                    // $("#image-container").css('display', 'none');
-                    // $("#image-container").slideUp();
+                    $("#image-container").slideUp();
                 }
             });
         } else {
             console.error(`Botones no encontrados: ${pair.yes} o ${pair.no}`);
         }
     });
+
+    /* Botones Alucinaciones*/
+    const btnYesAlucinaciones = document.getElementById('sintomas-positivos-alucinaciones-si');
+    const btnNoAlucinaciones = document.getElementById('sintomas-positivos-alucinaciones-no');
+    const btnNoSeDescartaAlucinaciones = document.getElementById('sintomas-positivos-alucinaciones-no-descarta');
+
+    if (btnYesAlucinaciones && btnNoAlucinaciones && btnNoSeDescartaAlucinaciones) {
+        btnYesAlucinaciones.addEventListener('click', function () {
+            btnYesAlucinaciones.classList.add('selected');
+            btnNoAlucinaciones.classList.remove('selected');
+            btnNoSeDescartaAlucinaciones.classList.remove('selected');
+            $("#tipo-alucinaciones").slideDown();
+        });
+
+        btnNoAlucinaciones.addEventListener('click', function () {
+            btnNoAlucinaciones.classList.add('selected');
+            btnYesAlucinaciones.classList.remove('selected');
+            btnNoSeDescartaAlucinaciones.classList.remove('selected');
+            $("#tipo-alucinaciones").slideUp();
+        });
+
+        btnNoSeDescartaAlucinaciones.addEventListener('click', function () {
+            btnNoSeDescartaAlucinaciones.classList.add('selected');
+            btnYesAlucinaciones.classList.remove('selected');
+            btnNoAlucinaciones.classList.remove('selected');
+            $("#tipo-alucinaciones").slideUp();
+        });
+    } else {
+        console.error('Uno o más botones de alucinaciones no encontrados.');
+    }
+    /*Fin Botones Alucinaciones*/
+
 }
 
 function determinar_diagnostico() {
@@ -121,69 +133,56 @@ function determinar_diagnostico() {
     const sexo = document.getElementById("sexo").value;
     const nombre = document.getElementById("nombre").value;
 
-    const transtornoAutista = document.querySelector("#transtorno-autista-si.selected") ? "Si" : document.querySelector("#transtorno-autista-no.selected") ? "No" : "";
-    const transtornoComunicacion = document.querySelector("#transtorno-comunicacion-si.selected") ? "Si" : document.querySelector("#transtorno-comunicacion-no.selected") ? "No" : "";
-    const transtornoEsquizoafectivo = document.querySelector("#transtorno-esquizoafectivo-si.selected") ? "Si" : document.querySelector("#transtorno-esquizoafectivo-no.selected") ? "No" : "";
-    const transtornoDepresivo = document.querySelector("#transtorno-depresivo-si.selected") ? "Si" : document.querySelector("#transtorno-depresivo-no.selected") ? "No" : "";
-    const transtornoBipolar = document.querySelector("#transtorno-bipolar-si.selected") ? "Si" : document.querySelector("#transtorno-bipolar-no.selected") ? "No" : "";
-    const antecedentesFamiliares = document.querySelector("#antecedentes-familiares-si.selected") ? "Si" : document.querySelector("#antecedentes-familiares-no.selected") ? "No" : "";
-    const estudios = document.querySelector("#estudios-si.selected") ? "Si" : document.querySelector("#estudios-no.selected") ? "No" : "";
-    const sustancias = document.querySelector("#sustancias-si.selected") ? "Si" : document.querySelector("#sustancias-no.selected") ? "No" : "";
-    // Obtener el valor de inmunodepresión
-    // const transtornoAutista = document.querySelector("#transtorno-autista-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#transtorno-autista-no.selected")
-    //         ? "No"
-    //         : "";
+    const transtornoAutista =
+        document.querySelector("#transtorno-autista-si.selected") ? "Si" :
+            document.querySelector("#transtorno-autista-no.selected") ? "No" : "";
+    const transtornoComunicacion =
+        document.querySelector("#transtorno-comunicacion-si.selected") ? "Si" :
+            document.querySelector("#transtorno-comunicacion-no.selected") ? "No" : "";
+    const transtornoEsquizoafectivo =
+        document.querySelector("#transtorno-esquizoafectivo-si.selected") ? "Si" :
+            document.querySelector("#transtorno-esquizoafectivo-no.selected") ? "No" : "";
+    const transtornoDepresivo =
+        document.querySelector("#transtorno-depresivo-si.selected") ? "Si" :
+            document.querySelector("#transtorno-depresivo-no.selected") ? "No" : "";
+    const transtornoBipolar =
+        document.querySelector("#transtorno-bipolar-si.selected") ? "Si" :
+            document.querySelector("#transtorno-bipolar-no.selected") ? "No" : "";
+    const antecedentesFamiliares =
+        document.querySelector("#antecedentes-familiares-si.selected") ? "Si" :
+            document.querySelector("#antecedentes-familiares-no.selected") ? "No" : "";
 
-    // // Obtener el valor de transtorno de comunicacion
-    // const transtornoComunicacion = document.querySelector("#transtorno-comunicacion-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#transtorno-comunicacion-no.selected")
-    //         ? "No"
-    //         : "";
+    // sintomas positivos            
+    const sintomasPositivosDuracion = document.getElementById("sintomas-positivos-duracion").value;
+    const sintomasPositivosAlucinaciones =
+        document.querySelector("#sintomas-positivos-alucinaciones-si.selected") ? "Si" :
+            document.querySelector("#sintomas-positivos-alucinaciones-no.selected") ? "No" :
+                document.querySelector("#sintomas-positivos-alucinaciones-no-descarta.selected") ? "No se descarta" :
+                    "";
+    const sintomasPositivosTipoAlucinaciones = document.getElementById("alucinaciones").value;
+    const sintomasPositivosTipoLenguaje = document.getElementById("lenguaje").value;
+    const sintomasPositivosTipoPensamiento = document.getElementById("pensamiento").value;
+    const sintomasPositivosTipoRitmoPensamiento = document.getElementById("ritmo-pensamiento").value;
+    const sintomasPositivosTipoContenidoPensamiento = document.getElementById("contenido-pensamiento").value;
+    const sintomasPositivosDelirios =
+        document.querySelector("#sintomas-positivos-delirios-presencia.selected") ? "Presencia" :
+            document.querySelector("#sintomas-positivos-delirios-ausencia.selected") ? "Ausencia" : "";
 
-    // // Obtener el valor de transtorno esquizoafectivo
-    // const transtornoEsquizoafectivo = document.querySelector("#transtorno-esquizoafectivo-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#transtorno-esquizoafectivo-no.selected")
-    //         ? "No"
-    //         : "";
+    // sintomas negativos
+    const sintomasNegativosDuracion = document.getElementById("sintomas-negativos-duracion").value;
+    const sintomasNegativosAspecto = document.getElementById("aspecto").value;
+    const sintomasNegativosAtencion = document.getElementById("atencion").value;
+    const sintomasNegativosActividad = document.getElementById("actividad").value;
+    const sintomasNegativosAfectividad = document.getElementById("afectividad").value;
 
-    // // Obtener el valor de transtorno depresivo
-    // const transtornoDepresivo = document.querySelector("#transtorno-depresivo-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#transtorno-depresivo-no.selected")
-    //         ? "No"
-    //         : "";
-
-    // // Obtener el valor de transtorno bipolar
-    // const transtornoBipolar = document.querySelector("#transtorno-bipolar-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#transtorno-bipolar-no.selected")
-    //         ? "No"
-    //         : "";
-    // // Obtener el valor de antecedentes familiares
-    // const antecedentesFamiliares = document.querySelector("#antecedentes-familiares-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#antecedentes-familiares-no.selected")
-    //         ? "No"
-    //         : "";
-
-    // // Obtener el valor de estudios
-    // const estudios = document.querySelector("#estudios-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#estudios-no.selected")
-    //         ? "No"
-    //         : "";
-
-    // // Obtener el valor de sustancias
-    // const sustancias = document.querySelector("#sustancias-si.selected")
-    //     ? "Si"
-    //     : document.querySelector("#sustancias-no.selected")
-    //         ? "No"
-    //         : "";
-
+    //complementarios
+    const sustancias =
+        document.querySelector("#sustancias-si.selected") ? "Si" :
+            document.querySelector("#sustancias-no.selected") ? "No" : "";
+    const estudios =
+        document.querySelector("#estudios-si.selected") ? "Si" :
+            document.querySelector("#estudios-no.selected") ? "No" : "";
+    const estudioCausaNatural = document.getElementById("estudio-causa-natural").value;
 
 
     // Crear un objeto con los valores recolectados
@@ -196,9 +195,26 @@ function determinar_diagnostico() {
         transtorno_esquizoafectivo: transtornoEsquizoafectivo,
         transtorno_depresivo: transtornoDepresivo,
         transtorno_bipolar: transtornoBipolar,
-        estudios: estudios,
-        sustancias: sustancias,
         antecedentes_familiares: antecedentesFamiliares,
+        // sintomas positivos,
+        sintomas_positivos_duracion: sintomasPositivosDuracion,
+        sintomas_positivos_alucinaciones: sintomasPositivosAlucinaciones,
+        sintomas_positivos_tipo_lenguaje: sintomasPositivosTipoLenguaje,
+        sintomas_positivos_tipo_alucinaciones: sintomasPositivosTipoAlucinaciones,
+        sintomas_positivos_tipo_pensamiento: sintomasPositivosTipoPensamiento,
+        sintomas_positivos_tipo_ritmo_pensamineto: sintomasPositivosTipoRitmoPensamiento,
+        sintomas_positivos_tipo_contenido_pensamiento: sintomasPositivosTipoContenidoPensamiento,
+        sintomas_positivos_delirios: sintomasPositivosDelirios,
+        // sintomas negativos,
+        sintomas_negativos_duracion: sintomasNegativosDuracion,
+        sintomas_negativos_aspecto: sintomasNegativosAspecto,
+        sintomas_negativos_atencion: sintomasNegativosAtencion,
+        sintomas_negativos_actividad: sintomasNegativosActividad,
+        sintomas_negativos_afectividad: sintomasNegativosAfectividad,
+        //complementarios
+        sustancias: sustancias,
+        estudios: estudios,
+        estudio_causa_natural: estudioCausaNatural
     };
 
     const jsonString = JSON.stringify(datosFormulario);
