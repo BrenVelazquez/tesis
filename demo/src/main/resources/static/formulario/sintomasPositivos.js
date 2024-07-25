6fbf145b901a9db75f88412ed2dae8000d94d8d3
Bren <brendavelazquez1121@gmail.com>
2024-07-24 21:13:17 -0300

    agrego multiselect

---
+++ demo/src/main/resources/static/formulario/sintomasPositivos.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Manejo del multiselect tipo-alucinaciones
+    const tipoAlucinacionesSelect = document.getElementById('alucinaciones');
+    if (tipoAlucinacionesSelect) {
+        tipoAlucinacionesSelect.addEventListener('change', function () {
+            const selectedOptions = Array.from(this.selectedOptions).map(option => option.value);
+            console.log('Tipos de alucinaciones seleccionadas: ', selectedOptions);
+            // TODO: REVISAR
+            // const selectedOptionsString = selectedOptions.join(', ') || 'Ninguna seleccionada';
+            // selectedAlucinationsLabel.textContent = `Seleccionadas: ${selectedOptionsString}`;
+            selectedOptions.forEach(option => {
+                const optionDiv = document.createElement('div');
+                optionDiv.className = 'selected-option';
+                optionDiv.innerHTML = `
+                    ${option.textContent}
+                    <button data-value="${option.value}">&times;</button>
+                `;
+                selectedOptionsContainer.appendChild(optionDiv);
+            });
+        
+        });
+    } else {
+        console.error('Elemento select no encontrado.');
+    }
+    const selectedOptionsContainer = document.getElementById('selected-options');
+
+
+});
