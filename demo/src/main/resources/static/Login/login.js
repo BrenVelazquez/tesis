f6efdc2994c8efd751f19986675bfc854d705e4c
Bren <brendavelazquez1121@gmail.com>
2024-10-26 20:05:43 -0300

    agrego validacion mail en login y ojo para mostrar/noMostrar la contraseña. Pequeños cambios visuales

--- demo/src/main/resources/static/Login/login.js
+++ demo/src/main/resources/static/Login/login.js
@@ -1,15 +1,35 @@
 let datosLogin = {};
 
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("contraseña");
+    const eyeIcon = document.getElementById("eyeIcon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
+
+
 function validar_campos(datosLogin) {
     resetearErrores();
-    document.getElementById("error-login").style.display='none';
+    document.getElementById("error-login").style.display = 'none';
     let esValido = true;
-
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (datosLogin.mail == "") {
         mostrarError(document.getElementById("mail"), "Por favor, ingrese su email.");
         esValido = false;
+    } else if (!emailPattern.test(datosLogin.mail)) {
+        mostrarError(document.getElementById("mail"), "Ingrese un email válido.");
+        esValido = false;
     }
+
     if (datosLogin.contraseña == "") {
         mostrarError(document.getElementById("contraseña"), "Por favor, ingrese su contraseña.");
         esValido = false;
@@ -21,7 +41,8 @@ function mostrarError(campo, mensaje) {
     campo.style.borderColor = "red";
     const errorDiv = document.createElement("div");
     errorDiv.className = "error-message";
-    errorDiv.style.color = "red";
+    errorDiv.style.color = "red"; 
+    errorDiv.style.marginBotton = "5px";
     errorDiv.textContent = mensaje;
     campo.parentNode.appendChild(errorDiv);
 }
@@ -45,18 +66,13 @@ function obtenerDatosLogin() {
     return datosLogin
 }
 
-
-
-
 async function iniciar_sesion() {
     const datosLogin = obtenerDatosLogin();
-    if (validar_campos(datosLogin)){
+    if (validar_campos(datosLogin)) {
         const jsonString = JSON.stringify(datosLogin);
-        console.log("Datos del formulario en formato JSON: ", jsonString);
-    
-        console.log('jsonString: ' + jsonString);
-
-        try{
+        // console.log("Datos del formulario en formato JSON: ", jsonString);
+        // console.log('jsonString: ' + jsonString);
+        try {
             const response = await $.ajax({
                 type: "POST",
                 url: "/login",
@@ -64,24 +80,18 @@ async function iniciar_sesion() {
                 data: jsonString,
             });
             if (response.mensaje === "Usuario y contraseña correcto.") {
-                const medico = response.medico; // Obtén el objeto Medico
-            
-                /*let newWindow=window.open("index.html", "_self");
-                newWindow.addEventListener('load', function(){
-                    newWindow.dniMedico = medico.dni;
-                  }, false);*/
+                const medico = response.medico;
+
                 console.log(response);
-                localStorage.setItem("medico",JSON.stringify(medico)); // Guarda el valor
-                //console.log("nombreMedico guardado:", localStorage.getItem('nombreMedico'));
-                window.location.href = "/home"; // Redirige a la nueva página
+                localStorage.setItem("medico", JSON.stringify(medico));
+                window.location.href = "/home";
                 return true;
             } else {
-                document.getElementById("error-login").style.display='block';
-    
+                document.getElementById("error-login").style.display = 'block';
                 return false;
             }
         } catch (error) {
             return false;
         }
-    } 
+    }
 }
